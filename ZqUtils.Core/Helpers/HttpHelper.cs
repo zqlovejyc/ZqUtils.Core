08f5ae924c29164ed4e8f8bc8e46acad40a975c4
zqlovejyc <943620963@qq.com>
2021-01-31 19:57:27 +0800

    重命名变量；

--- ZqUtils.Core/Helpers/HttpHelper.cs
+++ ZqUtils.Core/Helpers/HttpHelper.cs
@@ -722,8 +722,8 @@ namespace ZqUtils.Core.Helpers
         {
             var fi = new FileInfo(fileSavePath);
             var command = $" -c -s 5 --check-certificate=false -d {fi.DirectoryName} -o {fi.Name} " + url;
-            using var p = new Process();
-            CmdHelper.Execute(p, toolPath, command, (s, e) => action(e.Data));
+            using var process = new Process();
+            CmdHelper.Execute(process, toolPath, command, (s, e) => action(e.Data));
             return true;
         }
 
