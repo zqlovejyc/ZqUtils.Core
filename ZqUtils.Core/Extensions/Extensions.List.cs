44fa4dc77aef9d702284227ed3f38047eb69ebbc
zqlovejyc <943620963@qq.com>
2022-09-24 17:09:56 +0800

    修改参数抛出异常类型及提示内容；

--- ZqUtils.Core/Extensions/Extensions.List.cs
+++ ZqUtils.Core/Extensions/Extensions.List.cs
@@ -1802,7 +1802,7 @@ namespace ZqUtils.Core.Extensions
         public static int MaxIndex<T>(this IEnumerable<T> @this, out T maxValue) where T : IComparable<T>
         {
             if (@this.IsNullOrEmpty())
-                throw new ArgumentNullException(nameof(@this));
+                throw new ArgumentException($"`{nameof(@this)}` cannot be null or empty.");
 
             var index = 0;
             maxValue = @this.ElementAt(0);
@@ -1842,7 +1842,7 @@ namespace ZqUtils.Core.Extensions
         public static int MaxIndex<T>(this ICollection<T> @this, out T maxValue) where T : IComparable<T>
         {
             if (@this.IsNullOrEmpty())
-                throw new ArgumentNullException(nameof(@this));
+                throw new ArgumentException($"`{nameof(@this)}` cannot be null or empty.");
 
             var index = 0;
             maxValue = @this.ElementAt(0);
@@ -1883,7 +1883,7 @@ namespace ZqUtils.Core.Extensions
         public static int MinIndex<T>(this IEnumerable<T> @this, out T minValue) where T : IComparable<T>
         {
             if (@this.IsNullOrEmpty())
-                throw new ArgumentNullException(nameof(@this));
+                throw new ArgumentException($"`{nameof(@this)}` cannot be null or empty.");
 
             var index = 0;
             minValue = @this.ElementAt(0);
@@ -1923,7 +1923,7 @@ namespace ZqUtils.Core.Extensions
         public static int MinIndex<T>(this ICollection<T> @this, out T minValue) where T : IComparable<T>
         {
             if (@this.IsNullOrEmpty())
-                throw new ArgumentNullException(nameof(@this));
+                throw new ArgumentException($"`{nameof(@this)}` cannot be null or empty.");
 
             var index = 0;
             minValue = @this.ElementAt(0);
